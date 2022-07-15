ef92bde383c5166c5348df01e7598ad74c42848f
michaelyuhe <2312744987@qq.com>
2022-07-15 23:38:26 +0800

    add unique

--- debounce.js
+++ debounce.js
@@ -1,7 +1,11 @@
-function debounce (func, delay) {
+// debounce
+// 防抖，n 秒内再次触发则重新计时
+function debounce(func, delay) {
 	let timer = null
 	return function (...args) {
+		// 重新计时
 		clearTimeout(timer)
+		// 重新设置一个 delay 后触发的回调
 		timer = setTimeout(() => {
 			func.apply(this, args)
 		}, delay)
--- throttle.js
+++ throttle.js
@@ -1,9 +1,16 @@
-function throttle (func, interval) {
+// throttle 节流
+// 每隔一段时间才触发一次
+function throttle(func, interval) {
+	// 是否该节流
 	let toThrottle = false
 	return function (...args) {
-		if(!toThrottle) {
+		// 如果不需要节流了
+		if (!toThrottle) {
+			// 触发完就需要设置为需要节流
 			toThrottle = true
+			// 执行回调
 			func.apply(this, args)
+			// delay 后再取消节流
 			setTimeout(() => {
 				toThrottle = false
 			}, interval)
